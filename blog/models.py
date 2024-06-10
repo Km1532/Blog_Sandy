5ee4cb6536b796fa57198aea66e26554511ed3fa
Km1532 <oleksandr2606.kijanitskij@gmail.com>
2024-06-10 22:02:00 +0300

    Оновлення налаштувань та views.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField('Заговолок поста', max_length=100)
+    description = models.TextField('Опис поста')
+    author = models.CharField('Нік автора', max_length=100)
+    date = models.DateField('Дата публікації') 
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+
+    class Meta:
+        verbose_name = 'Запис'
+        verbose_name_plural = 'Записи'
